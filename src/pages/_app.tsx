4971adb0086df876a42f76da7ab6cc1261593e95
Fiquell <fiquellh@gmail.com>
2023-10-10 23:02:09 +0800

    adding material tailwind

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,9 +1,13 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import { type AppType } from "next/dist/shared/lib/utils";
-
 import "~/styles/globals.css";
 
 const MyApp: AppType = ({ Component, pageProps }) => {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 };
 
 export default MyApp;
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,14 @@
+import withMT from "@material-tailwind/react/utils/withMT";
 import { type Config } from "tailwindcss";
 
-export default {
-  content: ["./src/**/*.{js,ts,jsx,tsx}"],
+export default withMT({
+  content: [
+    "./src/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/components/**/*.{js,ts,jsx,tsx}",
+    "./node_modules/@material-tailwind/react/theme/components/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
     extend: {},
   },
   plugins: [],
-} satisfies Config;
+} satisfies Config);
