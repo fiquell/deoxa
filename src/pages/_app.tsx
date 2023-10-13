6addd503b4ce27737bf94ac4f7ddd95eba266399
Fiquell <fiquellh@gmail.com>
2023-10-14 02:43:19 +0800

    apply smooth scroll

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,25 @@
 import { ThemeProvider } from "@material-tailwind/react";
+import Lenis from "@studio-freight/lenis";
 import { type AppType } from "next/dist/shared/lib/utils";
+import { useEffect } from "react";
 import "~/styles/globals.css";
 
 const App: AppType = ({ Component, pageProps }) => {
+  useEffect(() => {
+    const lenis = new Lenis();
+
+    const raf = (time: number) => {
+      lenis.raf(time);
+      requestAnimationFrame(raf);
+    };
+
+    requestAnimationFrame(raf);
+
+    return () => {
+      lenis.destroy();
+    };
+  }, []);
+
   return (
     <ThemeProvider>
       <Component {...pageProps} />
