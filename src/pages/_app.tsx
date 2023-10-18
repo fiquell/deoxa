e0f4a60fa3124e0969ecbc2f7eeb99af63619fb5
Fiquell <fiquellh@gmail.com>
2023-10-19 04:27:58 +0800

    initial gsap

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,16 @@
 import { ThemeProvider } from "@material-tailwind/react";
 import Lenis from "@studio-freight/lenis";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 import { type AppType } from "next/dist/shared/lib/utils";
 import { useEffect } from "react";
 import "~/styles/globals.css";
 
 const App: AppType = ({ Component, pageProps }) => {
+  gsap.registerPlugin(ScrollTrigger);
+
   useEffect(() => {
-    const lenis = new Lenis({
-      duration: 1.5,
-    });
+    const lenis = new Lenis();
 
     const raf = (time: number) => {
       lenis.raf(time);
