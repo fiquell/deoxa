cf577acab870fff8308bb39e41d13334b5d68142
azmi <azmik5671@gmail.com>
2023-10-18 10:30:22 -0700

    already enchange about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -7,20 +7,20 @@ export default function About() {
       <div className="pt-12">
         <div className="flex flex-col gap-10 py-28 px-10 ">
           <div className="flex flex-col items-center justify-center">
-            <Typography variant="h3" className="pb-5">
+            <Typography variant="h3" className="mb-3">
               About <span className="text-primary">Us</span>
             </Typography>
-            <Typography variant="paragraph" className="lg:w-1/2 text-gray-500 text-center">
+            <Typography variant="paragraph" className="lg:w-1/2 text-center">
               Maecenas temTypography variant="paragraph"us tellus eget condimentum rhoncus sem quam semper
               libero pulvinar hendrerit id lorem.
             </Typography>
           </div>
-          <div className="container grid lg:grid-cols-3 place-items-center grid-cols-1 gap-5">
+          <div className="container grid md:grid-cols-3 lg:grid-cols-3 place-items-center grid-cols-1 gap-5">
             <div className="flex w-80 flex-col text-3xl">
               <Typography variant="h2">Lorem ipsum dolor sit amet consectetur</Typography>
             </div>
             <div className="flex w-80 flex-col">
-              <Typography variant="paragraph" className="text-xl mb-2">Lorem, ipsum </Typography>
+              <Typography variant="h5" className="text-xl mb-2">Lorem, ipsum </Typography>
               <Typography variant="paragraph" className="text-gray-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
                 itaque cupiditate similique ullam porro voluptatum, ex vero?
@@ -30,7 +30,7 @@ export default function About() {
               </Typography>
             </div>{" "}
             <div className="flex w-80 flex-col">
-              <Typography variant="paragraph" className="text-xl">Lorem, ipsum</Typography>
+              <Typography variant="h5" className="text-xl">Lorem, ipsum</Typography>
               <Typography variant="paragraph" className="text-gray-500">
                 Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque
                 itaque cupiditate similique ullam porro voluptatum, ex vero?
